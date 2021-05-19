285fbcc73419830b7e97a982eaab114cb0790b85
mdabdulhalimcse <mdabdulhalimcse@users.noreply.github.com>
2021-05-20 03:31:35 +0600

    traversing changed

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// GET ELEMENT BY ID
+// GET ELEMENTS BY TAGname
+// getElementsByClassName
 
 
-var element = document.getElementById('header1');
-
-
+// var element = document.getElementById('header1');
 //  element1 = element.innerText = 'todo apps';
 //  console.log(element1);
- 
 //  element1 = element.innerText ;
-element1 = element.textContent;
+// element1 = element.textContent;
 // console.log(element1);
 // element.style.color = 'red';
 // element.style.fontSize = '50px';
-console.log('element');
+// console.log('element');
+var element = document.getElementsByClassName('item')[0];
+console.log(element);
+element.style.color = 'red';
 
