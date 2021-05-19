37cf2d9643b362fa14d64e59e58d3a05830ed43f
mdabdulhalimcse <mdabdulhalimcse@users.noreply.github.com>
2021-05-20 03:17:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+var element = document.getElementById('header1');
+
+
+//  element1 = element.innerText = 'todo apps';
+//  console.log(element1);
+ 
+//  element1 = element.innerText ;
+element1 = element.textContent;
+// console.log(element1);
+// element.style.color = 'red';
+// element.style.fontSize = '50px';
+
